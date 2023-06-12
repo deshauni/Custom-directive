1fff05b05e63eedb9921ae127b12f20e030ddb5b
deshauni <98121733+deshauni@users.noreply.github.com>
2023-06-12 11:13:29 +0530

    2 files modified

--- src/app-ch-bg-color.directive.ts
+++ src/app-ch-bg-color.directive.ts
@@ -1,20 +1,26 @@
-import { Directive, ElementRef, Renderer2 ,OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2 , HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appAppChBgColor]'
 })
 export class AppChBgColorDirective {
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
-  constructor(private elementref: ElementRef, private renderer2: Renderer2) { 
-    this.changeBgColor();
+  @HostListener('mouseover') onMouseOver() {
+    this.backgroundColor = 'yellow';
+  }
+ @HostListener('mouseleave') onMouseLeave() {
+    this.backgroundColor = 'blue';
+  }  constructor(private elementref: ElementRef, private renderer2: Renderer2) { 
+    // this.changeBgColor();  custome directive
    }
+//code for custom directive
+// changeBgColor(){
+//   // this.renderer2.setStyle(this.elementref.nativeElement, 'color', 'red');
+//   this.elementref.nativeElement.style.color = 'red';
 
-changeBgColor(){
-  // this.renderer2.setStyle(this.elementref.nativeElement, 'color', 'red');
-  this.elementref.nativeElement.style.color = 'red';
 
-
-}
+//}
 
 
 }
